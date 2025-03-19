3d6408cb5c28beba8180a579c8853bc2c1d8d607
Drumil Shah <drumil.shah@fractal.ai>
2025-03-19 19:13:37 +0530

    path changes

--- tests/testing_fn.py
+++ tests/testing_fn.py
@@ -1,10 +1,9 @@
-from src import testing_operation
-import pytest
+from src import math_operation
 
 def test_add():
-    assert testing_operation.add(1,2) == 3
-    assert testing_operation.add(3,4) == 7
+    assert math_operation.add(1,2) == 3
+    assert math_operation.add(3,4) == 7
 
 def test_sub():
-    assert testing_operation.sub(1,2) == -1
-    assert testing_operation.sub(3,4) == -1
+    assert math_operation.sub(1,2) == -1
+    assert math_operation.sub(3,4) == -1
